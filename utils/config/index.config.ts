314ac400bf14667cf1ae69fdf85b3bdcb9a501e9
Joseph <oojoseph67@gmail.com>
2025-02-18 13:59:15 +0100

    chore: backurl update

--- utils/config/index.config.ts
+++ utils/config/index.config.ts
@@ -1,7 +1,8 @@
 import { http, createConfig } from "wagmi";
 import { sepolia } from "wagmi/chains";
 
-export const BACKEND_URL = "http://localhost:8888";
+// export const BACKEND_URL = "http://localhost:8888";
+export const BACKEND_URL = "https://cytric-backend-ocff.onrender.com";
 
 export const config = createConfig({
   chains: [sepolia],
